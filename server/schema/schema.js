572aaba5ed1502f140475302e3aabcfe4cb2303d
thanhdanh27600 <thanhdanh27600@gmail.com>
2022-08-28 17:52:10 +0700

    add mongo

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,4 +1,5 @@
-const {clients, projects} = require("../sampleData");
+const Client = require("../models/Client");
+const Project = require("../models/Project");
 
 const {
 	GraphQLObjectType,
@@ -18,20 +19,49 @@ const ClientType = new GraphQLObjectType({
 	}),
 });
 
+const ProjectType = new GraphQLObjectType({
+	name: "Project",
+	fields: () => ({
+		id: {type: GraphQLID},
+		name: {type: GraphQLString},
+		status: {type: GraphQLString},
+		description: {type: GraphQLString},
+		client: {
+			type: ClientType,
+			resolve(parent, arg) {
+				return Client.findById(parent.clientId);
+			},
+		},
+	}),
+});
+
 const RootQuery = new GraphQLObjectType({
 	name: "RootQueryType",
 	fields: {
 		clients: {
 			type: new GraphQLList(ClientType),
 			resolve(parents, args) {
-				return clients;
+				return Client.find();
 			},
 		},
 		client: {
 			type: ClientType,
 			args: {id: {type: GraphQLID}},
 			resolve(parent, args) {
-				return clients.find((client) => client.id == args.id);
+				return Client.findById(arg.id);
+			},
+		},
+		projects: {
+			type: new GraphQLList(ProjectType),
+			resolve(parents, args) {
+				return Project.find();
+			},
+		},
+		project: {
+			type: ProjectType,
+			args: {id: {type: GraphQLID}},
+			resolve(parent, args) {
+				return Project.findById(args.id);
 			},
 		},
 	},
